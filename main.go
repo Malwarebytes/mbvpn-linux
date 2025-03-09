51ea0fbdb5945ccf75c462d8cd4c4c6b7b0fa846
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2025-03-09 21:29:50 +0200

    Add systray integration sample

--- main.go
+++ main.go
@@ -8,5 +8,26 @@ import (
 )
 
 func main() {
-	cmd.Execute()
+	// systray.Run(onReady, onExit)
+  cmd.Execute()
 }
+
+// func onReady() {
+// 	systray.SetIcon(icon.Data)
+// 	systray.SetTitle("MBVPN")
+// 	systray.SetTooltip("Malwarebytes VPN")
+// 	mQuit := systray.AddMenuItem("Quit", "Quit the app")
+//
+// 	go func() {
+// 		<-mQuit.ClickedCh
+// 		systray.Quit()
+// 	}()
+//
+// 	cmd.Execute()
+// }
+//
+// func onExit() {
+// }
+
+// Environment to run the systray app
+// nix-shell -p libgcc gtk3 libindicator-gtk3 libayatana-appindicator
