af0fc25a0c61f0e360161d290bba1b4b2b6e781d
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2025-05-28 21:28:22 +0300

    Update package names for tests

--- test/integration/holocron_test.go
+++ test/integration/holocron_test.go
@@ -1,20 +1,3 @@
 //go:build integration
 
-package main
-
-import (
-	"os"
-)
-
-const (
-	machineId = "JDfhsjdfKDJfhgdj8fsf3"
-)
-
-// getLicenseKey returns the license key from environment variable
-func getLicenseKey() string {
-	licenseKey := os.Getenv("MBVPN_TEST_LICENSE_KEY")
-	if licenseKey == "" {
-		panic("MBVPN_TEST_LICENSE_KEY environment variable is required but not set")
-	}
-	return licenseKey
-}
+package integration_test
