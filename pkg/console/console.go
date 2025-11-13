81c0a59a6630d47f1475d8e5d579206921a11f75
Vladimir Parfenov <parfenov.vladimir.s@gmail.com>
2025-11-13 16:07:57 +0200

    refactor: remove RunCmd from console.go

--- pkg/console/console.go
+++ pkg/console/console.go
@@ -8,39 +8,7 @@ import (
 	"strings"
 
 	"github.com/malwarebytes/mbvpn-linux/pkg/config"
-	log "github.com/sirupsen/logrus"
 )
-
-// RunCmd executes a terminal command, optionally with sudo.
-func RunCmd(sudo bool, command string, args ...string) (string, error) {
-	var cmd *exec.Cmd
-
-	var stdout strings.Builder
-	var stderr strings.Builder
-
-	if sudo {
-		// Check if sudo is available
-		_, err := exec.LookPath("sudo")
-		if err != nil {
-			log.Debugln("Sudo not available, running without sudo...")
-			cmd = exec.Command(command, args...)
-		} else {
-			cmd = exec.Command("sudo", append([]string{command}, args...)...)
-		}
-	} else {
-		cmd = exec.Command(command, args...)
-	}
-
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run()
-	if err != nil {
-		return stdout.String(), fmt.Errorf("command execution failed: %w, stderr: %v, stdout: %v", err, stderr.String(), stdout.String())
-	}
-
-	return stdout.String(), nil
-}
 
 func WgShow() (string, error) {
 	var stdout strings.Builder
@@ -103,6 +71,7 @@ func WgDown(cfgPath string, dirProvider config.DirectoryProvider) error {
 	return nil
 }
 
+// TODO: avoid passing dirProvider here
 func sanitizeWgConfigPath(cfgPath string, dirProvider config.DirectoryProvider) (string, error) {
 	cleanPath := cfgPath
 
